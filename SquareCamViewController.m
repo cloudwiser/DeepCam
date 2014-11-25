9d6053139cae0e012156a94abb3549e21b0da974
cloudwise <nickhall@Mac-mini-2.home>
2014-11-25 00:52:40 +0000

    Fixed iOS 8.x iCloud document saving

--- SquareCamViewController.m
+++ SquareCamViewController.m
@@ -1141,6 +1141,7 @@ bail:
         
         // ...and move the new predictor file to iCloud
         __block BOOL success = NO;
+        writableDBPath = [@"file:///" stringByAppendingString:writableDBPath];  // Fix for iOS 8 iCloud Documents?
         dispatch_async (dispatch_get_global_queue (DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^(void) {
             NSURL *sourceURL = [NSURL URLWithString:writableDBPath];
             NSFileManager *fileManager = [[[NSFileManager alloc] init] autorelease];
@@ -1157,6 +1158,7 @@ bail:
         });
         return success;
     } else {
+        NSLog(@"Write failed : fp = %@", fp);
         return NO;
     }
 }
@@ -1267,6 +1269,7 @@ bail:
     
     // ...if it does, move it to the local directory container
     __block BOOL wasMoved = NO;
+    writableDBPath = [@"file:///" stringByAppendingString:writableDBPath];  // Fix for iOS 8 iCloud Documents?
     dispatch_async (dispatch_get_global_queue (DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^(void) {
         NSURL *destURL = [NSURL URLWithString:writableDBPath];
         NSFileManager *fileManager = [[[NSFileManager alloc] init] autorelease];
